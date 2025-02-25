b669bab4ed29e24d68c48fb5fa259646b841a7e1
athebest36521 <526atiwari@frhsd.com>
2025-02-25 09:29:43 -0500

    Finished Day 1 Tutorial

---
+++ script.js
@@ -0,0 +1,13 @@
+//Init(create) empty array element called tasks
+let tasks = [];
+
+//Add an ON CLICK event listener to the "add task button" that calls a function called addTask
+document.getElementById("addTaskBtn").addEventListener("click", function() {
+    //Get the value of the input field and store it in a variable called taskInput
+    let taskInput = document.getElementById("taskInput").value;
+
+    //Check if the taskInput has something in it
+    if (taskInput) {
+        //Create a new object called task with the following properties
+    }
+});
